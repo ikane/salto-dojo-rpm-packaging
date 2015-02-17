c1e850452b21ba03600953d3d764fb76c2c878f5
Quentin Vandekerckhove <qvandekerckhove@salto-consulting.com>
2015-02-18 00:48:14 +0100

    Add a log message.

--- deamon-application/src/main/java/com/saltoconsulting/dojo/deamon/service/HelloWorldService.java
+++ deamon-application/src/main/java/com/saltoconsulting/dojo/deamon/service/HelloWorldService.java
@@ -1,16 +1,24 @@
 package com.saltoconsulting.dojo.deamon.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloWorldService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldService.class);
+	
 	public String hello() {
 		return hello("World");
 	}
 	
 	public String hello(String who) {
-		return "Hello " + who;
+		String message = "Hello " + who;
+		if(LOGGER.isInfoEnabled()) {
+			LOGGER.info(message);
+		}
+		return message;
 	}
 	
 }
