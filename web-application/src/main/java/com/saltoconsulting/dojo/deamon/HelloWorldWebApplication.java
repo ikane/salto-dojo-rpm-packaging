33a4b31461a67824c04351c552e1468034275e93
Quentin Vandekerckhove <qvandekerckhove@salto-consulting.com>
2015-02-21 00:53:41 +0100

    Fix build compatible with both embedded and external web container

--- web-application/src/main/java/com/saltoconsulting/dojo/deamon/HelloWorldWebApplication.java
+++ web-application/src/main/java/com/saltoconsulting/dojo/deamon/HelloWorldWebApplication.java
@@ -2,13 +2,20 @@ package com.saltoconsulting.dojo.deamon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HelloWorldWebApplication extends SpringBootServletInitializer {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldWebApplication.class, args);
 	}
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldWebApplication.class);
+	}
+
 }
--- web-application/src/main/java/com/saltoconsulting/dojo/deamon/controller/HelloWorldController.java
+++ web-application/src/main/java/com/saltoconsulting/dojo/deamon/controller/HelloWorldController.java
@@ -1,21 +1,25 @@
 package com.saltoconsulting.dojo.deamon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.saltoconsulting.dojo.deamon.service.HelloWorldService;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
 	@Autowired
 	private HelloWorldService helloWorldService;
 
-	@RequestMapping("hello")
-	@ResponseBody
+	@RequestMapping("/")
+	public String welcome(
+			@RequestParam(defaultValue = "man", required = false, value = "who") String who) {
+		return this.helloWorldService.hello(who);
+	}
+	
+	@RequestMapping("/hello")
 	public String helloWorld(
 			@RequestParam(defaultValue = "World", required = false, value = "who") String who) {
 		return this.helloWorldService.hello(who);
