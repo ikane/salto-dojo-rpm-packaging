16487dd87678d5f5afc04e70f8564b4bc1d46820
Quentin Vandekerckhove <qvandekerckhove@salto-consulting.com>
2015-02-20 15:55:18 +0100

    Add build of a webapplication. Test of drone.io

---
+++ web-application/src/main/java/com/saltoconsulting/dojo/deamon/HelloWorldWebApplication.java
@@ -0,0 +1,14 @@
+package com.saltoconsulting.dojo.deamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloWorldWebApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWebApplication.class, args);
+	}
+	
+}
